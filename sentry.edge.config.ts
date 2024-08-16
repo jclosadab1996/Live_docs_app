7418ae739be3bbe84f4e6ff7adb38b478b43664b
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2024-08-15 23:03:24 -0500

    Correciones

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,7 +6,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://e300b41df2ffb6d4592972400a25c0d3@o4506876178464768.ingest.us.sentry.io/4507159179034624",
+  dsn: "https://30f6a272ba64bfc88adf274261cc5eb3@o4507738763952128.ingest.us.sentry.io/4507784665497600",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
